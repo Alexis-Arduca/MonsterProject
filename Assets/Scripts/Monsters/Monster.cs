e5776e856ed9876591b9a71e93b762f6ebccd6c9
Fizzey <alexis.arduca@epitech.eu>
2025-05-07 15:20:51 +0200

    [ADD] - Monster Element
    Adding element for the monster

--- Assets/Scripts/Monsters/Monster.cs
+++ Assets/Scripts/Monsters/Monster.cs
@@ -3,9 +3,14 @@ using System;
 
 public class Monster : MonoBehaviour
 {
+    public enum ElementType { Null, Fire, Ice, Electric, Psychic }
+
     [Header("Description")]
     [SerializeField] protected string monsterName;
     [SerializeField] protected string description;
+
+    [Header("Parameters")]
+    [SerializeField] protected ElementType element = ElementType.Null;
 
     [Header("Power")]
     [SerializeField] protected Power power;
