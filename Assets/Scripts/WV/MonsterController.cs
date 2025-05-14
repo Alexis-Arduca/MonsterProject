7404c5b4df5a54e7d60dca03168d2f7a3a54e7fb
Viro Neo <van-do.pham@epitech.eu>
2025-05-14 13:36:15 +0200

    :sparkles: added NavMesh to follow the player

--- Assets/Scripts/WV/MonsterController.cs
+++ Assets/Scripts/WV/MonsterController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class MonsterController : MonoBehaviour
@@ -6,13 +7,16 @@ public class MonsterController : MonoBehaviour
     private ThoughtBubbleController _thoughtBubble;
     public Image wantedItem;
     private PickableController _pickableController;
+    private NavMeshAgent _agent;
+    public Transform target;
 
     private void Start()
     {
         _thoughtBubble = GetComponentInChildren<ThoughtBubbleController>();
         _thoughtBubble.ShowBubble();
         _thoughtBubble.SetWantedItem(wantedItem);
-
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.autoBraking = true;
     }
 
     public void Interact(PickableController item)
@@ -21,6 +25,7 @@ public class MonsterController : MonoBehaviour
         {
             _thoughtBubble.HideBubble();
             item.Destroy();
+            _agent.SetDestination(target.position);
         }
     }
 }
