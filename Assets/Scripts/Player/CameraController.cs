062baea3d9a81f4d06ac4c2dea6f1bbefab93671
Fizzey <alexis.arduca@epitech.eu>
2025-05-26 14:45:33 +0200

    [UPDATE] - Handle controller
    Now the game can be played using a controller (Xbox or something else). Also cleaning a bit the repository

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -1,19 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraZoom : MonoBehaviour
 {
     private bool playerAction = false;
-    public float mouseSensitivity = 500f;
     public Transform playerBody;
     public PlayerMovement playerMovement;
 
     private float xRotation = 0f;
 
+    private PlayerControls controls;
+    private Vector2 lookInput;
+    private readonly float mouseSensitivity = 0.5f;
+    private readonly float gamepadSensitivity = 150f;
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+
+        controls = new PlayerControls();
+        controls.Gameplay.Enable();
+
+        controls.Gameplay.Look.performed += ctx => lookInput = ctx.ReadValue<Vector2>();
+        controls.Gameplay.Look.canceled += ctx => lookInput = Vector2.zero;
 
         if (playerMovement == null && playerBody != null)
         {
@@ -23,30 +32,44 @@ public class CameraZoom : MonoBehaviour
                 Debug.LogError("PlayerMovement does not exist !");
             }
         }
+
         GameEventsManager.instance.loreEvents.onImportantLoreEvent += ChangeAction;
     }
 
     void OnDisable()
     {
         GameEventsManager.instance.loreEvents.onImportantLoreEvent -= ChangeAction;
+        controls.Gameplay.Disable();
     }
 
     private void ChangeAction()
     {
         playerAction = !playerAction;
 
-        // if (playerAction == true) {
-        //     Cursor.lockState = CursorLockMode.None;
-        // } else {
-        //     Cursor.lockState = CursorLockMode.Locked;
-        // }
+        if (playerAction) {
+            Cursor.lockState = CursorLockMode.None;
+        } else {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 
     void Update()
     {
-        if (!playerAction) {
-            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        if (!playerAction)
+        {
+            float mouseX, mouseY;
+
+            if (Mouse.current != null && Mouse.current.delta.IsActuated())
+            {
+                mouseX = lookInput.x * mouseSensitivity;
+                mouseY = lookInput.y * mouseSensitivity;
+            }
+            else
+            {
+                mouseX = lookInput.x * gamepadSensitivity * Time.deltaTime;
+                mouseY = lookInput.y * gamepadSensitivity * Time.deltaTime;
+            }
+
 
             xRotation -= mouseY;
             xRotation = Mathf.Clamp(xRotation, -90f, 90f);
@@ -57,10 +80,6 @@ public class CameraZoom : MonoBehaviour
             if (playerMovement != null)
             {
                 playerMovement.HandleMovement(transform);
-            }
-            else
-            {
-                Debug.LogWarning("PlayerMovement does not exist !");
             }
         }
     }
--- Assets/Scripts/Player/PlayerControler.cs
+++ Assets/Scripts/Player/PlayerControler.cs
@@ -1,17 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.InputSystem.Utilities;
 
 public class PlayerControler : MonoBehaviour
 {
     private bool playerAction = false;
     private PlayerMovement playerMovement;
+    public PlayerControls controls;
 
     void Start()
     {
+        controls = new PlayerControls();
+        controls.Gameplay.Enable();
         playerMovement = GetComponent<PlayerMovement>();
+
         GameEventsManager.instance.loreEvents.onImportantLoreEvent += ChangeAction;
+
+        controls.Gameplay.Jump.performed += ctx => {
+            if (!playerAction) playerMovement.HandleJump();
+        };
+        
+        controls.Gameplay.Sprint.performed += ctx => playerMovement.HandleSprint();
+        controls.Gameplay.Sprint.canceled += ctx => playerMovement.HandleSprint();
     }
 
     void OnDisable()
@@ -24,7 +33,6 @@ public class PlayerControler : MonoBehaviour
         if (!playerAction)
         {
             playerMovement.HandleMovement(Camera.main.transform);
-            playerMovement.HandleJump();
         }
     }
 
