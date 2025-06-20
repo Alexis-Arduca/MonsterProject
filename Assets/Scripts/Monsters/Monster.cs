ec8318b00f3285ca8cdd2ac5e808ccaf11f78e9e
Fizzey <alexis.arduca@epitech.eu>
2025-06-20 15:42:55 +0200

    [ADD] - NavMesh link
    Use for Monster jump from a platform to another

--- Assets/Scripts/Monsters/Monster.cs
+++ Assets/Scripts/Monsters/Monster.cs
@@ -21,7 +21,7 @@ public class Monster : MonoBehaviour
     [SerializeField] protected List<ElementType> immunity = new List<ElementType>();
     [SerializeField] protected bool isFriendly = false;
     [SerializeField] protected BiomesTemplate.BiomeType spawnBiome;
-    protected State currentState;
+    [SerializeField] protected State currentState;
     protected Rigidbody rb;
     private static readonly Vector3[] directions = { Vector3.forward, Vector3.back, Vector3.left, Vector3.right };
 
